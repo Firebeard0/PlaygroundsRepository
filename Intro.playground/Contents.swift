c3c5996f0ad7890b8df35691a116fffadb3ad716
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-09-25 12:43:37 -0600

    made variables

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -5,3 +5,12 @@ import UIKit
 var myName :String
 var otherName = "words"
 var thisName :String = "thor"
+RETURN; thisName
+
+print (thisName)
+var number = 14 / 2 + 5 % 2
+print(number)
+if number == 8
+{
+    print(thisName)
+}
