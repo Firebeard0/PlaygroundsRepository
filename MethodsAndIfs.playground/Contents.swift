2e1bdf6227250a0426198630fd2691359d2b2867
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-09-27 11:54:11 -0600

    made functions and called it

--- MethodsAndIfs.playground/Contents.swift
+++ MethodsAndIfs.playground/Contents.swift
@@ -6,3 +6,13 @@ public func isReallySimple()  -> Void
 {
     print("it will be very nice this weekend!!!! yay for 70+ degrees")
 }
+isReallySimple()
+
+public func asBitLessSimple(name: String) -> Void
+{
+    let answer = "My Name is Not " + name
+    print(answer)
+}
+asBitLessSimple(name: "Slim Shad")
+let words = "Marshall Mathers"
+asBitLessSimple(name: words)
