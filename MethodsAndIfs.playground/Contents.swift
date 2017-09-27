4a4168c5835eed70e23689c47238752c5ff8adc9
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-09-27 12:51:38 -0600

    added more functions and a while loop

--- MethodsAndIfs.playground/Contents.swift
+++ MethodsAndIfs.playground/Contents.swift
@@ -16,3 +16,33 @@ public func asBitLessSimple(name: String) -> Void
 asBitLessSimple(name: "Slim Shad")
 let words = "Marshall Mathers"
 asBitLessSimple(name: words)
+
+public func namingExample(outername innername :String) -> Void
+{
+    print("the inner name is: \(innername)")
+}
+namingExample(outername: "Noah")
+
+public func counting() -> Int
+{
+  return 7
+}
+print("I am not \(counting()) years old")
+
+if(counting() < 10)
+{
+    print("math works in swift too")
+}
+else
+{
+    print("not very likely now")
+}
+var test = 0
+while (test < counting())
+{
+    print("hahhahaasshasfadf")
+    test += 1
+}
+
+print("yay")
+
