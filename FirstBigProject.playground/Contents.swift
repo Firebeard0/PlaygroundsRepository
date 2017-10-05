2302365af4a5c4b34f975685ebad88eb092e5cf4
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-10-05 12:53:56 -0600

    added getter and uis

--- FirstBigProject.playground/Contents.swift
+++ FirstBigProject.playground/Contents.swift
@@ -2,4 +2,19 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var topic = "working with  gui"
+
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
+
+myFirstButton.setTitle("words", for: .normal)
+
+myFirstButton.backgroundColor = .green
+
+var mySecondButton =  UIButton(frame: CGRect(x:25, y:40 ,width: 590, height: 300))
+mySecondButton.setTitle("hello", for: .normal)
+var myLabelI = UILabel(frame: CGRect(x:56, y:34 ,width: 546, height: 564))
+var myLabelII = UILabel(frame: CGRect(x:23, y:34 ,width: 567, height: 345))
+var myVeiwI = UIView(frame: CGRect(x:12, y:34,width: 456, height: 456))
+var myVeiwII = UIView(frame: CGRect(x:23, y:45,width: 356, height: 345))
+var myVeiwIII = UIView(frame: CGRect(x:34, y:56,width: 567, height: 235))
+var myDatePicker = UIDatePicker(frame: CGRect(x:23 ,y:34 , width: 234, height: 798))
